70d55e7bd38a3f838bd48719cf77c54cff80db55
Tpakmopucm <explay0907@gmail.com>
2022-06-21 00:32:03 +0500

    finish app

--- src/components/Watches.js
+++ src/components/Watches.js
@@ -1,26 +1,83 @@
-import moment from "moment";
+import { useState, useRef } from "react";
+import WatchesItem from "./WatchesItem";
+import { v4 } from 'uuid';
 
 const Watches = () => {
-  return (
-  <form className="AppForm">
-    <div className="InputWrapper">
-      <label htmlFor="Title" className="formLabel">
-        Название
-      </label>
-      <input name="Title">
-      </input>
-    </div>
-    <div className="InputWrapper">
-      <label htmlFor="Title" className="formLabel">
-        Временная зона
-      </label>
-      <input name="Title" type="number" min="-12" max="14">
-      </input>
-    </div>
-    <button type="submit" className="formButton">Добавить</button>
-    <span>{moment().utcOffset(-23).format('HH:mm:ss')}</span>
-  </form>
-  )
-}
+  const [name, setName] = useState("");
+  const [offset, setOffset] = useState();
+  const [timeArray, setTimeArray] = useState([]);
 
-export default Watches
+  const nameRef = useRef();
+  const offsetRef = useRef();
+
+  const onChangeHandler = (evt) => {
+    const { value } = evt.target;
+    evt.target.name === 'name' ? setName(value) : setOffset(Number(value));
+  };
+
+  const testData = (name, offset) => {
+    if (nameRef.current.value === "" || offsetRef.current.value === "") {
+      alert("Введите данные");
+      return true;
+    }
+  };
+
+  const onSubmitHandler = (evt) => {
+    evt.preventDefault();
+    if (testData(name, offset)) {
+      return;
+    }
+    const newTimeArray = [...timeArray]
+
+    newTimeArray.push({timeName: name, timeOffset: offset, id: v4()})
+    setTimeArray(newTimeArray);
+    setName('');
+    setOffset();
+    nameRef.current.value = '';
+    offsetRef.current.value = '';
+
+  };
+
+  const HandleRemove = (id) => {
+    setTimeArray((prevTimeArray) => prevTimeArray.filter((o) => o.id !== id));
+  }
+
+  return (
+    <div className="appWrapper">
+    <form className="AppForm" onSubmit={onSubmitHandler}>
+      <div className="InputWrapper">
+        <label htmlFor="name" className="formLabel">
+          Название
+        </label>
+        <input 
+          name="name" 
+          ref={nameRef}
+          onChange={onChangeHandler}
+          ></input>
+      </div>
+      <div className="InputWrapper">
+        <label htmlFor="offset" className="formLabel">
+          Временная зона
+        </label>
+        <input
+          name="offset"
+          type="number"
+          min="-12"
+          max="14"
+          ref={offsetRef}
+          onChange={onChangeHandler}
+          placeholder={"UTC offset"}
+        ></input>
+      </div>
+      <button type="submit" className="formButton">
+        Добавить
+      </button>
+    </form>
+    <div className="watchItems">
+      {timeArray.map(item => <WatchesItem name={item.timeName} offset={item.timeOffset} id={item.id} key={item.id} onRemove={HandleRemove}/>)}
+    </div>
+    </div>
+  );
+};
+
+export default Watches;
--- src/components/WatchesItem.js
+++ src/components/WatchesItem.js
@@ -1,11 +1,37 @@
-const WatchesItem = () => {
-  return (
-    <div>
-      <span></span>
-      <span></span>
-      <button>✘</button>
-    </div>
-  )
-}
+import moment from "moment";
+import { useState, useEffect } from "react";
 
-export default WatchesItem
+const WatchesItem = (props) => {
+  const [time, setTime] = useState(moment().utcOffset(props.offset).format("HH:mm:ss"));
+  let timerId;
+
+  const Watch = () => {
+    console.log('tick')
+    return setTime(moment().utcOffset(props.offset).format("HH:mm:ss"))
+  }
+
+  const handleRemove = () => {
+    props.onRemove(props.id)
+  }
+
+  useEffect( () => {
+    timerId = setInterval(Watch, 1000)
+
+    return () => {
+      clearInterval(timerId)
+    }
+  }, [time])
+
+  return (
+    <div className="watchItem">
+      <div className="watchItem__content">
+        <span className="watchName">{props.name}</span>
+        <div className="watchTime">{time}</div>
+      </div>
+      <button className="watchBtn" onClick={handleRemove}>✘</button>
+    </div>
+  );
+};
+
+export default WatchesItem;
+
